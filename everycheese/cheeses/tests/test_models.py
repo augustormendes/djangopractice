1721bb4c1dce24f0cdc810d5092defccbfda48d8
augustormendes <augusto_r_mendes@hotmail.com>
2021-05-04 09:50:40 -0300

    Factory in test

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,12 +1,8 @@
 import pytest
 from ..models import Cheese
 pytestmark = pytest.mark.django_db
-
+from .factories import CheeseFactory
 def test__str__():
-    cheese = Cheese.objects.create(
-        name="Stracchino",
-        description="Semi-sweet cheese that goes well with starches.",
-        firmness=Cheese.Firmness.SOFT
-    )
-    assert cheese.__str__() == "Stracchino"
-    assert str(cheese) == "Stracchino"
+    cheese = CheeseFactory()
+    assert cheese.__str__() == cheese.name
+    assert str(cheese) == cheese.name
